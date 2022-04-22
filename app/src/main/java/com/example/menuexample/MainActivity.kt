ebdd9d6bc35f65fd720ba1368b29cf205b8caa52
ZakiRahman <mzakiriau@gmail.com>
2022-04-22 13:39:36 +0700

    end MenuExample

---
+++ app/src/main/java/com/example/menuexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.menuexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
